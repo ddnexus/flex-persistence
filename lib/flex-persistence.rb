532e8a2cd4589b4308ec33e57301e98fd868c823
Domizio Demichelis <dd.nexus@gmail.com>
2012-11-06 11:46:45 +0100

    added persistence_method.yml

--- lib/flex-persistence.rb
+++ lib/flex-persistence.rb
@@ -5,3 +5,13 @@ require 'flex/persistence/finders'
 require 'flex/class_proxy/stored_model'
 require 'flex/instance_proxy/stored_model'
 require 'flex/stored_model'
+
+module Flex
+  module Persistence
+    extend self
+
+    include Loader
+    flex.load_search_source  File.expand_path('../flex/persistence_methods.yml', __FILE__)
+
+  end
+end
