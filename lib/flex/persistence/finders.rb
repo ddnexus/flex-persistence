41356f12efa2f0bbab5793068f5d719dea5682ce
Domizio Demichelis <dd.nexus@gmail.com>
2012-11-11 09:09:04 +0100

    finders refactoring

--- lib/flex/persistence/finders.rb
+++ lib/flex/persistence/finders.rb
@@ -5,13 +5,13 @@ module Flex
 
       #    MyModel.find(ids)
       #    - ids can ba a single id or an array of ids
-      #
+
       #    MyModel.find '1Momf4s0QViv-yc7wjaDCA'
       #      #=> #<MyModel ... color: "red", size: "small">
-      #
+
       #    MyModel.find ['1Momf4s0QViv-yc7wjaDCA', 'BFdIETdNQv-CuCxG_y2r8g']
       #      #=> [#<MyModel ... color: "red", size: "small">, #<MyModel ... color: "bue", size: "small">]
-      #
+
       def find(ids, vars={})
         result = if ids.is_a?(Array)
                    Flex.multi_get(process_vars(:ids => ids).deep_merge(vars))
@@ -21,105 +21,48 @@ module Flex
         flex_result(result, vars)
       end
 
-      #    MyModel.all(vars={})
-      #
-      #    vars = { # accepts also :any_term => nil for missing values
-      #             :terms   => {:field_one => 'something', :field_two => nil},
-      #             # accepts one or an array of sort structures documented in http://www.elasticsearch.org/guide/reference/api/search/sort.html
-      #             :sort    => {:field_three => :desc},
-      #             # accepts one or an array of filter structures
-      #             :filters => {:range => {:created_at => {:from => 2.days.ago, :to => Time.now}}
-      #             # other params passed to ES (in case of :fields the records will be frozen, and the missing fileds will be nil)
-      #             :params => {:fields => 'field_one,field_two,field_three'} }
-      #
-      #    # will retrieve all documents, the results will be limited by the default :size param
-      #    MyModel.all(vars_or_nil)
-      #
-      def all(vars={})
-        variables = process_vars(vars)
-        result    = Persistence.find variables
-        flex_result(result, vars)
-      end
 
-      #    MyModel.scan_all(vars={}, &block)
-      #    - vars are the same kind of Hash described for the method :all
-      #    - scan_search: the block will be yielded many times with an array of batched results.
-      #      You can pass :scroll and :size as :params in order to control the action.
-      #      See http://www.elasticsearch.org/guide/reference/api/search/scroll.html
-      #    MyModel.all(vars_or_nil) do |batch_of_results|
-      #      do_something_with batch_of_results
-      #    end
+      #    scoped methods. They returns a Scoped object similar to AR.
+      #    You can chain scopes, then you can call :count, :first, :all and :scan_all to get your result
+      #    This provides an alternative API to the variables hash. See Flex::Persistence::Scoped
       #
-      def scan_all(vars={}, &block)
-        variables = process_vars(vars)
-        result    = flex.scan_search(Persistence.flex.templates[:find], variables, &block)
-        flex_result(result, vars)
-      end
-
-      #    MyModel.first(vars={})
-      #    - vars are the same kind of Hash described for the method :all
-      #    - it limits the size of the query to 1 and returns it as a single document object
+      #    scoped = MyModel.terms(:field_one => 'something', :field_two => nil)
+      #                    .sort(:field_three => :desc)
+      #                    .filters(:range => {:created_at => {:from => 2.days.ago, :to => Time.now})
+      #                    .fields('field_one,field_two,field_three')
+      #                    .params(:any => 'param')
       #
-      def first(vars={})
-        vars = Variables.new(vars).deep_merge(:params => {:size => 1})
-        all(vars).first
-      end
-
-      #    MyModel.count(vars={})
-      #    - will return the count
-      #    - vars are the same kind of Hash described for the method :all
+      #    # add another filter or other terms at any time
+      #    scoped2 = scoped.terms(...).filters(...)
       #
-      def count(vars={})
-        result = Persistence.flex.count_search(:find, process_vars(vars))
-        result['hits']['total']
-      end
-
-      # scoped methods. They returns a Scoped object similar to AR.
-      # You can chain scopes, then you can call :count, :first, :all and :scan_all to get your result
-      # This provides an alternative API to the variables hash. See Flex::Persistence::Scoped
+      #    scoped2.count
+      #    scoped2.first
+      #    scoped2.all
+      #    scoped2.scan_all {|batch| do_something_with_results batch}
       #
-      #
-      #  scoped = MyModel.terms(:field_one => 'something', :field_two => nil)
-      #                  .sort(:field_three => :desc)
-      #                  .filters(:range => {:created_at => {:from => 2.days.ago, :to => Time.now})
-      #                  .fields('field_one,field_two,field_three')
-      #                  .params(:any => 'param')
-      #
-      #  # add another filter or other terms at any time
-      #  scoped2 = scoped.terms(...).filters(...)
-      #
-      #  scoped2.count
-      #  scoped2.first
-      #  scoped2.all
-      #  scoped2.scan_all {|res| do_something_with_results}
-
-
-      [:terms, :params, :filters, :sort, :fields].each do |meth|
-        define_method(meth) do |value|
-          Scoped.new(self).send(meth, value)
-        end
-      end
+      delegate :terms, :params, :filters, :sort, :fields, :to => :scoped
 
       # You can start with a non restricted Flex::Persistence::Scoped object
       def scoped
         Scoped.new(self)
       end
 
-    private
-
-      def process_vars(vars)
-        terms = process_terms(vars.delete(:terms))
-        flex.variables.deep_merge(vars, terms)
-      end
-
-      def process_terms(terms)
-        return unless terms
-        raise ArgumentError, "Unexpected argument (got #{terms.inspect})" \
-              unless terms.is_a?(Hash)
-        clean_terms    = {}
-        missing_fields = []
-        terms.each { |f, v| v.nil? ? missing_fields.push({ :missing => f }) : (clean_terms[f] = v) }
-        {:terms => clean_terms, :_missing_fields => missing_fields}
+      #    Alternative hash based API
+      #    Instead of chaining scopes (terms, sort, ...) and then call the query method (fist, all, scan_all, count)
+      #    you can directly pass an hash of variables to the query methods
+      #    See Flex::Persistence::Scoped
+      #
+      #    MyModel.count(vars_or_nil)
+      #    MyModel.first(vars_or_nil)
+      #    MyModel.all(vars_or_nil)
+      #    MyModel.scan_all(vars_or_nil){|batch| do_something_with_results batch}
+      #
+      [:count, :first, :all, :scan_all].each do |meth|
+        define_method(meth) do |vars, &block|
+          terms = Scoped.process_terms(vars.delete(:terms)||{})
+          vars  = flex.variables.deep_merge(vars, terms)
+          scoped.replace(vars).send(meth, &block)
+        end
       end
 
     end
--- lib/flex/persistence/scoped.rb
+++ lib/flex/persistence/scoped.rb
@@ -4,49 +4,64 @@ module Flex
 
       include Structure::Mergeable
 
-      def initialize(model)
-        @model_class = model
+      # never instantiate this object directly: it is automatically done by the StoredModel.scoped method
+      def initialize(model_class)
+        @model_class = model_class
         replace @model_class.flex.variables
       end
 
+      # accepts also :any_term => nil for missing values
       def terms(value)
-        deep_merge! process_terms(value)
+        deep_merge! self.class.process_terms(value)
         self
       end
 
+      # the standard :params variable
       def params(value)
         deep_merge! :params => value
         self
       end
 
+      # accepts one or an array of filter structures
       def filters(value)
         process_array :filters, value
         self
       end
 
+      # accepts one or an array of sort structures documented in http://www.elasticsearch.org/guide/reference/api/search/sort.html
+      # doesn't support the multiple hash form, but you can pass an hash as single argument or an array of hashes
       def sort(value)
         process_array :sort, value
         self
       end
 
+      # the fields that you want to retrieve (limiting the size of the response)
+      # the returned records will be frozen, and the missing fileds will be nil
+      # pass a comma separated (no space) string e.g. 'field_one,field_two'
       def fields(value)
         params(:fields => value)
         self # just for simmetry
       end
 
+      # it limits the size of the query to 1 and returns it as a single document object
       def first
         result = Persistence.find params(:size => 1)
         @model_class.flex_result(result, self).first
       end
 
+      # will retrieve all documents, the results will be limited by the default :size param
+      # use #scan_all if you want to really retrieve all documents (in batches)
       def all
         result = Persistence.find self
         @model_class.flex_result(result, self)
       end
 
+      # scan_search: the block will be yielded many times with an array of batched results.
+      # You can pass :scroll and :size as params in order to control the action.
+      # See http://www.elasticsearch.org/guide/reference/api/search/scroll.html
       def scan_all(&block)
         result = @model_class.flex.scan_search(Persistence.flex.templates[:find], self, &block)
-        @model_class.flex_result(result, vars)
+        @model_class.flex_result(result, self)
       end
 
       def count
@@ -58,9 +73,9 @@ module Flex
         "#<#{self.class.name} #{self}>"
       end
 
-      private
+    private
 
-      def process_terms(hash)
+      def self.process_terms(hash)
         clean_terms    = {}
         missing_fields = []
         hash.each { |f, v| v.nil? ? missing_fields.push({ :missing => f }) : (clean_terms[f] = v) }
