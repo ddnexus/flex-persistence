60fb4052879fbe3b3bc5c45b7191efe553fbc79d
Domizio Demichelis <dd.nexus@gmail.com>
2012-12-04 23:10:12 +0100

    implemented flex.get_index_mapping

--- lib/flex/class_proxy/stored_model.rb
+++ lib/flex/class_proxy/stored_model.rb
@@ -12,6 +12,25 @@ module Flex
         super
       end
 
+      def get_index_mapping
+        props = { }
+        context.attributes.each do |name, attr|
+          options     = attr.send(:options)
+          props[name] = case
+                        when options.has_key?(:properties)
+                          Utils.keyfy(:to_s, attr.send(:options)[:properties])
+                        when options.has_key?(:not_analyzed) && options[:not_analyzed] ||
+                            options.has_key?(:analyzed) && !options[:analyzed]
+                          { 'type' => 'string', 'index' => 'not_analyzed' }
+                        when options[:type] == DateTime
+                          { 'type' => 'date', 'format' => 'dateOptionalTime' }
+                        else
+                          next
+                        end
+        end
+        {index => { 'mappings' => { type => { 'properties' => props } } } } unless props.empty?
+      end
+
     end
   end
 end
